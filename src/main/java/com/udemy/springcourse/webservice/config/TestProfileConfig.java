e06dab0897268a710988927c43244c142ff3980f
ruanvalente <ruanopensuselinux@gmail.com>
2022-07-03 00:10:37 -0300

    chore: add seed for order

--- src/main/java/com/udemy/springcourse/webservice/config/TestProfileConfig.java
+++ src/main/java/com/udemy/springcourse/webservice/config/TestProfileConfig.java
@@ -1,12 +1,15 @@
 package com.udemy.springcourse.webservice.config;
 
+import com.udemy.springcourse.webservice.entities.Order;
 import com.udemy.springcourse.webservice.entities.User;
+import com.udemy.springcourse.webservice.repository.OrderRepository;
 import com.udemy.springcourse.webservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -15,6 +18,8 @@ public class TestProfileConfig implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
 
 
     @Override
@@ -22,6 +27,11 @@ public class TestProfileConfig implements CommandLineRunner {
         User user1 = new User(null, "Freeman Romana", "freeman.romana@gmail.com", "99999999", "11929292");
         User user2 = new User(null, "Ada Rauha", "ada.rauha@gmail.com", "55999299", "2291928");
 
+        Order order1 = new Order(null, Instant.parse("2022-06-20T19:53:07Z"), user1);
+        Order order2 = new Order(null, Instant.parse("2022-07-21T03:42:10Z"), user2);
+        Order order3 = new Order(null, Instant.parse("2022-07-22T15:21:22Z"), user1);
+
         userRepository.saveAll(Arrays.asList(user1, user2));
+        orderRepository.saveAll(Arrays.asList(order1, order2, order3));
     }
 }
