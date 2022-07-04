f24d40f4ca23f047eacf088288cb8e844ac19732
ruanvalente <ruanopensuselinux@gmail.com>
2022-07-04 17:52:23 -0300

    feat: add association in category class

--- src/main/java/com/udemy/springcourse/webservice/entities/Category.java
+++ src/main/java/com/udemy/springcourse/webservice/entities/Category.java
@@ -2,7 +2,9 @@ package com.udemy.springcourse.webservice.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -14,6 +16,9 @@ public class Category implements Serializable {
     private Long id;
 
     private String name;
+
+    @Transient
+    private final Set<Product> products = new HashSet<>();
 
     public Category() { }
 
@@ -39,6 +44,10 @@ public class Category implements Serializable {
         this.name = name;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -59,4 +68,6 @@ public class Category implements Serializable {
                 ", name='" + name + '\'' +
                 '}';
     }
+
+
 }
