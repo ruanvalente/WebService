7e5b2ef5ca547923adcfc8f45ac66800f6328e40
ruanvalente <ruanopensuselinux@gmail.com>
2022-07-04 15:26:50 -0300

    feat: add orderStatus class

--- src/main/java/com/udemy/springcourse/webservice/entities/Order.java
+++ src/main/java/com/udemy/springcourse/webservice/entities/Order.java
@@ -1,6 +1,7 @@
 package com.udemy.springcourse.webservice.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.udemy.springcourse.webservice.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -19,16 +20,19 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order () {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -46,6 +50,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+       if (orderStatus != null) {
+           this.orderStatus = orderStatus.getCode();
+       }
     }
 
     public User getClient() {
